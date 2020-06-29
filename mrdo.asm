8c58f3726453a371b672d0b9836e6265478fbc66
CARIAS <JulioCariasB>
2020-06-28 19:18:25 -0600

    podar cesped

--- mrdo.asm
+++ mrdo.asm
@@ -10,10 +10,54 @@ start:
 show_mr_do:
     push ebp
     mov ebp, esp
-    sub esp, 4
-    mov dword [ebp-4], 0x1e021e01 ; Current mr_do
+    sub esp, 4 
+
+    mov ecx, 0xb800
+for_field:
+    cmp ecx, 0xcabf
+    je end_for_field
+    mov dword [ecx], 0xa22ea22e
+    inc ecx
+    jmp for_field
+
+end_for_field:  
+    mov dword [ebp-4], 0x44024401 ; Current mr_do mitad arriba
+    mov dword [ebp-8], 0x44024401 ; Current mr_do mitad abajo
+    mov ecx, dword[ebp+8]
+    mov dword [ebp-12], ecx ;ultima row
+    mov ecx, dword[ebp+12]
+    mov dword [ebp-16], ecx ;ultima Column
+    
    
 $loop:
+    ;borrado mitad arriba
+    mov esi, dword [ebp-12] ; Row
+    mov ebx, esi
+    shl esi, 6
+    shl ebx, 4
+    add esi, ebx
+    add esi, dword [ebp-16] ; Col
+    shl esi, 1
+    add esi, 0xb800
+    
+    mov ebx, 0
+    mov dword [esi], ebx
+
+    ;borrado mitad abajo
+    mov esi, dword [ebp-12] ; Row
+    inc esi
+    mov ebx, esi
+    shl esi, 6
+    shl ebx, 4
+    add esi, ebx
+    add esi, dword [ebp-16] ; Col
+    shl esi, 1
+    add esi, 0xb800
+    
+    mov ebx, 0
+    mov dword [esi], ebx
+
+    ;mitad arriba
     mov esi, dword [ebp+8] ; Row
     mov ebx, esi
     shl esi, 6
@@ -25,6 +69,20 @@ $loop:
     
     mov ebx, dword [ebp-4]
     mov dword [esi], ebx
+
+    ;mitad abajo
+    mov esi, dword [ebp+8] ; Row
+    inc esi
+    mov ebx, esi
+    shl esi, 6
+    shl ebx, 4
+    add esi, ebx
+    add esi, dword [ebp+12] ; Col
+    shl esi, 1
+    add esi, 0xb800
+    
+    mov ebx, dword [ebp-8]
+    mov dword [esi], ebx
  
     push 50
     call delay
@@ -33,15 +91,22 @@ $loop:
 $test_keys:
     mov ah, byte [0xffff0004] ; Keypad
     mov al, ah
-   
+
     ; Left key
     and al, 0x1
     test al, al
     jz $test_right
- 
-    mov dword [ebp-4], 0x1e041e03 ; mr_do left
+
+    mov ecx, dword[ebp+8]
+    mov dword [ebp-12], ecx ;ultima row
+    mov ecx, dword[ebp+12]
+    mov dword [ebp-16], ecx ;ultima Column
+    
+    mov dword [ebp-4], 0x44024401 ; mr_do left mitad arriba
+    mov dword [ebp-8], 0x44024401 ; mr_do left mitad abajo
     cmp dword [ebp+ 12], 0  ; collipsion left wall
     je $loop
+
     sub dword [ebp+ 12], 1
     jmp $loop
        
@@ -50,8 +115,14 @@ $test_right:
     and al, 0x2
     test al, al
     jz $test_down
+
+    mov ecx, dword[ebp+8]
+    mov dword [ebp-12], ecx ;ultima row
+    mov ecx, dword[ebp+12]
+    mov dword [ebp-16], ecx ;ultima Column
    
-    mov dword [ebp-4], 0x1e021e01 ; mr_do right
+    mov dword [ebp-4], 0x44024401 ; mr_do right mitad arriba
+    mov dword [ebp-8], 0x44024401 ; mr_do right mitad abajo
     cmp dword [ebp+ 12], 79  ; collipsion right wall
     je $loop
     add dword [ebp+ 12], 1
@@ -62,8 +133,14 @@ $test_down:
     and al, 0x4
     test al, al
     jz $test_up
+    
+    mov ecx, dword[ebp+8]
+    mov dword [ebp-12], ecx ;ultima row
+    mov ecx, dword[ebp+12]
+    mov dword [ebp-16], ecx ;ultima Column
    
-    mov dword [ebp-4], 0x1e081e07 ; mr_do up
+    mov dword [ebp-4], 0x44024401 ; mr_do up mitad arriba
+    mov dword [ebp-8], 0x44024401 ; mr_do up mitad abajo
     cmp dword [ebp+ 8], 29  ; collipsion upper wall
     je $loop
     add dword [ebp+ 8], 1
@@ -74,8 +151,14 @@ $test_up:
     and al, 0x8
     test al, al
     jz $test_q
+    
+    mov ecx, dword[ebp+8]
+    mov dword [ebp-12], ecx ;ultima row
+    mov ecx, dword[ebp+12]
+    mov dword [ebp-16], ecx ;ultima Column
    
-    mov dword [ebp-4], 0x1e061e05 ; mr_do up
+    mov dword [ebp-4], 0x44024401 ; mr_do dpwn mitad arriba
+    mov dword [ebp-8], 0x44024401 ; mr_do dpwn mitad abajo
     cmp dword [ebp+ 8], 0  ; collipsion down wall
     je $loop
     sub dword [ebp+ 8], 1
@@ -85,6 +168,12 @@ $test_q:
     mov al, ah
     and al, 0x10
     test al, al
+    
+    mov ecx, dword[ebp+8]
+    mov dword [ebp-12], ecx ;ultima row
+    mov ecx, dword[ebp+12]
+    mov dword [ebp-16], ecx ;ultima Column
+
     jnz $end_loop
    
     jmp $test_keys
